8c0a98e811cba31188060985a9ba32367ec6ef3f
Naveen soni <nsoni8102@gmail.com>
2024-03-04 09:21:00 +0530

    learn about tailwind and props

---
+++ React_3/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/Card";
+const App = () => {
+  return (
+    <>
+      <div className=" flex ">
+        <Card title={"First"} />
+        <Card title={"Seconed"} />
+      </div>
+    </>
+  );
+};
+
+export default App;
