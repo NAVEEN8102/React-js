8fd6f6c9feb61988629666b69663bd82a2936bbc
Naveen soni <nsoni8102@gmail.com>
2024-03-12 09:49:07 +0530

    learn more about context api

---
+++ React_7/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+import { ThemeProvide } from "./context/ThemeContext";
+
+const App = () => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode]);
+  return (
+    <ThemeProvide value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvide>
+  );
+};
+
+export default App;
